26ed238245532f7b802f597e530cb0438e679a1c
kedG0 <ked240@gmail.com>
2022-08-16 22:39:08 -0500

    Clase 5 completa
    Se actualiza todo el proceso seguido en la clase 5.

--- src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
+++ src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
@@ -2,9 +2,18 @@ package com.UdeA.Ciclo3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
 public class Ciclo3Application {
+
+	@GetMapping("/hello")	//Al entrar a la ruta http://localhost:8080/hello se ejecuta lo siguiente:
+	public String hello(){
+		return "Hola ciclo 3. Este es el primer print en web";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ciclo3Application.class, args);
