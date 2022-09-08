d69e815bdbce579e28de240ba949115455b73f28
kedG0 <ked240@gmail.com>
2022-09-08 08:26:15 -0500

    Template y servicios para agregar nueva empresa.

--- src/main/java/com/UdeA/Ciclo3/controller/Controlador.java
+++ src/main/java/com/UdeA/Ciclo3/controller/Controlador.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -31,4 +33,13 @@ public class Controlador {
         model.addAttribute("empr", emp);
         return "agregarEmpresa";
     }
+
+    @PostMapping("/GuardarEmpresa") //Se usa Post porque vamos a crear un nuevo elemento, llevar info y setearla.
+    public String guardarEmpresa(Empresa empr, RedirectAttributes redirectAttributes){
+        if(empresaService.saveOrUpdateEmpresa(empr)==true){
+            return "redirect:/VerEmpresas";
+        }
+        return "redirect:/AgregarEmpresa";
+    }
+
 }
